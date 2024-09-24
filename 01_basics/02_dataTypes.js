bba65c9e30d3026e9b43b3763d7dc04bbb5f1e2f
margish babariya <babariyamargish97@gmail.com>
2024-09-24 08:29:46 +0530

    new learning add

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,16 +1,62 @@
-let name = "Margish"
-let age = 28
-let isLoggedIn = false
+// Primitive
+
+// 8 Types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+let stringType = "Margish Patel" // String
+let numberType = 123456789 //Number
+let booleanType = true // Boolean
+let nullType = null //null
+let undefinedType = undefined //undefined
+let symbolTypeId = Symbol("Margish") //symbol
+let bigintType = 123456789n //bigint
+
+// console.log(typeof bigintType);
+
+
+//Reference (Non primitive) 
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "Margish",
+    age: 28,
+}
+
+// const myFunction = function(){
+//     console.log("Hello world...!")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let name = "Margish"
+// let age = 28
+// let isLoggedIn = false
 
 // number => 2 to power 53
 // bigint
 // string => ""
 // boolean => true/false
 // null => standalone value
-// undefiend => 
+// undefiend =>
 // symbol => unique
 
 
 //object
 
-console.log(typeof "Margish");
+// console.log(typeof "Margish");
+
+
+
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "margish"
+const age = 28
+
+// console.log(name + age + " You are adult");
+
+// console.log(`My name is ${name} and my age is ${age}`);
+
+const gameName = new String('margishpatel')
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("r"));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-12, 8)
+// console.log(anotherString);
+
+
+const newStringOne = "   margish  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://margish.com/home%20page"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('margish'));
+console.log(url.split('/'));
+
