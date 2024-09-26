610f13a14ba8a7c36a29ddeb1f1589324b69f426
margish babariya <babariyamargish97@gmail.com>
2024-09-26 08:21:12 +0530

    learning about strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -17,12 +17,34 @@ const gameName = new String('margishpatel')
 
 
 const newStringOne = "   margish  "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
 const url = "https://margish.com/home%20page"
 
-console.log(url.replace('%20', '-'));
-console.log(url.includes('margish'));
-console.log(url.split('/'));
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('margish'));
+// console.log(url.split('/'));
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++//
+
+let str = "My name is Margish Patel, My vilage name is Dhoraji"
+
+// let a = str.length;
+// let a = str.toLowerCase();
+// let a = str.toUpperCase();
+// let a = str.includes("margish");
+// let a = str.startsWith("My");
+// let a = str.endsWith("Dhoraji");
+
+// let a = str.search("Margish"); // 11th position of this carrecter // if we search wong carrecter then it's return is -1
+
+// let a = str.match(/is/g);
+// let a = str.lastIndexOf("is");
+let a = str.replace("Dhoraji", "AMD");
+
+console.log(a);
+// console.log(typeof(a)); 
 
