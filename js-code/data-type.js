63f93d3ed4a0f6922a730e70422e17ae0c14612b
margish babariya <babariyamargish97@gmail.com>
2024-10-15 08:24:11 +0530

    new code add

---
+++ js-code/data-type.js
@@ -0,0 +1,21 @@
+//string
+let str = "margish patel"
+
+//number
+let num = 123
+
+//boolen
+let bul1 = true
+let bul2 = false
+
+//array
+let x = ["margish", "nipa", "viraj"]
+
+//object
+let obj = {
+    name: "margish patel",
+    age: 28,
+    cgpa: 7.5,
+    isPass: true,
+}
+
