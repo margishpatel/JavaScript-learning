5c5a4207e2adacb647278313123012d7c17cab2b
Margish Patel <margish.emedstore@gmail.com>
2024-11-06 20:01:15 +0530

    js

--- js-code/practice-questions.js
+++ js-code/practice-questions.js
@@ -258,6 +258,12 @@
 
 
 // let nameArray = ["margish", "chetan", "dhrumil", "raj", "viraj", "kaushik"];
+
+// console.log(nameArray[0]);
+// console.log(nameArray.at(0));
+// console.log(nameArray[2]);
+
+
 // if (nameArray.length > 5) {
 //   console.log(nameArray, "array has more than 5 elements");
 
@@ -278,9 +284,139 @@
 //   console.log("Yeh ek vowel nahi hai.");
 // }
 
-//************************************************************//
+//************************* 02_day_data_types ****************************//
+
+//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge = "30 Days Of JavaScript"
+
+//Print the string on the browser console using console.log()
+console.log(challenge);
+
+//Print the length of the string on the browser console using console.log()
+console.log(challenge.length);
+
+//Change all the string characters to capital letters using toUpperCase() method
+console.log(challenge.toUpperCase());
+
+//Change all the string characters to lowercase letters using toLowerCase() method
+console.log(challenge.toLowerCase());
+
+//Cut (slice) out the first word of the string using substr() or substring() method
+console.log(challenge.substring(0, 2));
+
+//Slice out the phrase "Days Of JavaScript" from "30 Days Of JavaScript".
+console.log(challenge.slice(3));
+
+//Check if the string contains a word Script using includes() method
+console.log(challenge.includes("Script"));
+
+//Split the string into an array using split() method
+console.log(challenge.split(" "));
+console.log(challenge.replace(" ", "-"));
+console.log(challenge.replaceAll(" ", "-").split("-").slice(-1));
+console.log(challenge.replaceAll(" ", "-").split("-").pop());
+
+//'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+let str = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(str.split(","));
+
+//Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+console.log(str.replace("Facebook", "Emed"));
+
+//What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+console.log(challenge.charAt(15)); // S
+
+//What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+console.log(challenge.charCodeAt(3));
+
+//Use indexOf to determine the position of the first occurrence of "a" in "30 Days Of JavaScript".
+console.log(challenge.indexOf('a')); //4
+
+//Use lastIndexOf to determine the position of the last occurrence of "a" in "30 Days Of JavaScript".
+console.log(challenge.lastIndexOf('a')) //14
+
+//Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let str2 = "You cannot end a sentence with because because because is a conjunction";
+console.log(str2.indexOf("because")); //31
+
+//Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(str2.lastIndexOf("because")); //47
+
+//Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(str2.search('because')); // 31
+
+//Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+let str3 = " 30 Days Of JavaScript "
+console.log(str3.trim(" ")); // Only starting and ending space will remove from the string
+
+//Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.startsWith("30")); // true
+
+//Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.endsWith("JavaScript"));  // true
+
+//Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log(challenge.match(/a/g));
+
+//Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+let str4 = "30 Days of"
+let str5 = " JavaScript"
+console.log(str4.concat(str5));
+
+//Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log(challenge.repeat(2));
 
 
+
+//Level 2
+
+//Using console.log() print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+console.log(`The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`);
+
+//Using console.log() print out the following quote by Mother Teresa: "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+
+console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`);
+
+//Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+let str6 = "10";
+let str7 = 10;
+let num1 = Number(str6);
+let num2 = str7.toString();
+
+console.log(num1 === 10); //true
+console.log(num2 === "10"); //true
+
+
+//I hope this course is not full of jargon. Check if jargon is in the sentence.
+let sentence = 'I hope this course is not full of jargon.';
+console.log(sentence.includes('jargon')); // true
+
+//Generate a random number between 0 and 100 inclusively.
+let randomNum = Math.floor(Math.random() * 1001); // 101 kyunki 0-100 inclusive chahiye
+console.log(randomNum);
+
+//Generate a random number between 50 and 100 inclusively.
+let randomNum2 = Math.floor(Math.random() * 51) + 50; // 50-100 inclusive range ke liye
+console.log(randomNum2);
+
+
+//Generate a random number between 0 and 255 inclusively.
+let randomNum3 = Math.floor(Math.random() * 256); // 0-255 inclusive
+console.log(randomNum3);
+
+//Access the 'JavaScript' string characters using a random number.
+let str8 = "JavaScript"
+let rendNumb1 = Math.floor(Math.random() * 10)
+console.log(rendNumb1);
+console.log(str8.charAt(rendNumb1));
+
+//Use console.log() and escape characters to print the following pattern.
+console.log("1\t1\t1\t1\t1\n2\t1\t2\t4\t10");
+
+//'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+let sentence2 = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+let loveCount = sentence2.match(/love/gi).length; // 'gi' se case-insensitive search hota hai
+console.log(loveCount); // 3
 
 //************************************************************//
 
